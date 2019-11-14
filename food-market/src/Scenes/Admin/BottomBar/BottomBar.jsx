b9bf38ce2e127c1fd1fe45ec0ad6f51898c0bc8f
Inti <yeseniase@outlook.it>
2019-11-14 00:10:07 -0600

    ADD: delete icon

--- food-market/src/Scenes/Admin/BottomBar/BottomBar.jsx
+++ food-market/src/Scenes/Admin/BottomBar/BottomBar.jsx
@@ -4,6 +4,7 @@ import {ReactComponent as AddIcon} from '../../../assets/Images/icons/add-icon.s
 import {ReactComponent as Logo} from '../../../assets/Images/logo.svg';
 import {ReactComponent as EditIcon} from '../../../assets/Images/icons/edit-icon.svg';
 import {ReactComponent as SaveIcon} from '../../../assets/Images/icons/save-icon.svg';
+import {ReactComponent as DeleteIcon} from '../../../assets/Images/icons/delete-icon.svg';
 import {Link} from 'react-router-dom';
 
 const BottomBar = ()=>{
@@ -11,6 +12,9 @@ const BottomBar = ()=>{
         <div className='BottomBar'>
             <div className="BottomBar-item">
                 <AddIcon/>
+            </div>
+            <div className="BottomBar-item">
+                <DeleteIcon/>
             </div>
             <div className="BottomBar-item-logo BottomBar-item">
                 <Link to ={'/a/recipe'}>
